7a572e332142272cb2342129bfd947caaaeac009
lifesci <ericdito@gmail.com>
2023-11-10 17:15:58 -0800

    Simplify evaluation of binary operations; begin work on statement evaluation

--- src/Eval.hs
+++ src/Eval.hs
@@ -4,7 +4,13 @@ import Parser
 import Data.Map(Map)
 import qualified Data.Map as Map
 
-data Scope = Scope (Map String Int) (Maybe Scope)
+data GameState = GameState {
+    currentRoom :: String,
+    rooms :: Map String RoomNode,
+    scope :: Scope
+} deriving Show
+
+data Scope = Scope (Map String Int) (Maybe Scope) deriving Show
 
 emptyScope :: Scope
 emptyScope = (Scope Map.empty Nothing)
@@ -16,7 +22,7 @@ addScope :: Scope -> Scope
 addScope x = (Scope Map.empty (Just x))
 
 parentScope :: Scope -> Maybe Scope
-parentScope (Scope x y) = y
+parentScope (Scope _ y) = y
 
 searchScope :: Scope -> String -> Int
 searchScope (Scope vars Nothing) var = case (Map.lookup var vars) of
@@ -32,19 +38,57 @@ boolToInt x = if x then 1 else 0
 intToBool :: Int -> Bool
 intToBool x = x /= 0
 
+evalStmt :: StmtNode -> GameState -> GameState
+evalStmt (DeclareNode d) game = GameState {
+    currentRoom=(currentRoom game),
+    rooms=(rooms game),
+    scope=(evalDeclare (declareVar d) (declareVal d) (scope game))
+}
+evalStmt (AssignNode a) game =  GameState {
+    currentRoom=(currentRoom game),
+    rooms=(rooms game),
+    scope=evalDeclare (assignVar a) (assignVal a) (scope game)
+}
+evalStmt x y = error "Not implemented"
+
+evalDeclare :: String -> ExprNode -> Scope -> Scope
+evalDeclare var val scope = addScopeVar scope var (evalExpr val scope)
+
+and' :: Int -> Int -> Int
+and' x y = boolToInt ((intToBool x) && intToBool y)
+
+or' :: Int -> Int -> Int
+or' x y = boolToInt ((intToBool x) || intToBool y)
+
+gt :: Int -> Int -> Int
+gt x y = boolToInt (x > y)
+
+lt :: Int -> Int -> Int
+lt x y = boolToInt (x < y)
+
+gte :: Int -> Int -> Int
+gte x y = boolToInt (x >= y)
+
+lte :: Int -> Int -> Int
+lte x y = boolToInt (x <= y)
+
 evalExpr :: ExprNode -> Scope -> Int
-evalExpr (BinOpNode op x y) scope = case op of
-    Add -> evalExpr x scope + evalExpr y scope
-    Sub -> evalExpr x scope - evalExpr y scope
-    Mul -> evalExpr x scope * evalExpr y scope
-    Div -> evalExpr x scope `div` evalExpr y scope
-    Mod -> evalExpr x scope `mod` evalExpr y scope
-    Or -> boolToInt ((intToBool (evalExpr x scope)) || (intToBool (evalExpr y scope)))
-    And -> boolToInt ((intToBool (evalExpr x scope)) && (intToBool (evalExpr y scope)))
-    Gt -> boolToInt ((evalExpr x scope) > (evalExpr y scope))
-    Lt -> boolToInt ((evalExpr x scope) < (evalExpr y scope))
-    Gte -> boolToInt ((evalExpr x scope) >= (evalExpr y scope))
-    Lte -> boolToInt ((evalExpr x scope) <= (evalExpr y scope))
+evalExpr (BinOpNode op x y) scope = let
+    f = case op of
+        Add -> (+)
+        Sub -> (-)
+        Mul -> (*)
+        Div -> div
+        Mod -> mod
+        Or -> or'
+        And -> and'
+        Gt -> gt
+        Lt -> lt
+        Gte -> gte
+        Lte -> lte
+    in
+        f (evalExpr x scope) (evalExpr y scope)
+
 evalExpr (UnOpNode op x) scope = case op of
     Neg -> -(evalExpr x scope)
     Not -> boolToInt (not (intToBool (evalExpr x scope)))
